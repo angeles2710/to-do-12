e907083fb443ca00187010b19da14b2997f44f99
angeles2710 <116809719+angeles2710@users.noreply.github.com>
2022-11-08 22:27:21 +0100

    Create Opc1.jsx

---
+++ src/todo/Opc1.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Opc1() {
+  function ShowTD() {}
+
+  return (
+    <>
+      <h1>List To Do</h1>
+      <p>User Id</p>
+      <button type="button" onClick={ShowTD}>
+        ShowTD
+      </button>
+    </>
+  );
+}
